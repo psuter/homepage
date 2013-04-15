f085367268ceab4ba4dddee4163edf711b78427b
Philippe Suter <philippe.suter@gmail.com>
2013-04-15 15:15:33 +0200

    nothing really

--- app/controllers/Homepage.scala
+++ app/controllers/Homepage.scala
@@ -4,8 +4,9 @@ import play.api._
 import play.api.mvc._
 
 object Homepage extends Controller {
-  
-  def index = Action {
+  def index = Action { implicit request =>
+    // Not quite ready for prime time...
+    // Ok(views.html.home())
     Redirect("http://lara.epfl.ch/~psuter/")
   }
   
